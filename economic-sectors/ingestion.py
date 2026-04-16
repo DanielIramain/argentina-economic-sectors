778dbc2ddc7bdb3d736f0782064614f298dcbfdd
Dardo Daniel Iramain <danieliramain97@gmail.com>
2026-04-16 14:07:50 +0000

    Cloud: setup and implementation

--- economic-sectors/ingestion.py
+++ economic-sectors/ingestion.py
@@ -4,19 +4,25 @@
 import io
 import os
 
-import dlt
-import pandas as pd
-import requests
+from translate_dicts import map_list
 
-# Configuraciones
+import dlt
+import requests
+import pandas as pd
+
+# Configurations
 OWNER = "DanielIramain"
 REPO = "argentina-economic-sectors"
 TAG = "v1.0.0"
 SECTORS = ['apicola', 'tabaco', 'te', 'trigo', 'maiz', 'energias-alternativas', 'arroz', 'algodonera-textil', 'fruticola-fruta-de-carozo', 'construccion', 'forestal-papel-y-muebles', 'vitivinicultura', 'telecomunicaciones', 'comercio-interno', 'automotriz-y-autopartista', 'maquinaria-agricola', 'servicios-turisticos', 'mineria-metalifera-y-rocas-de-aplicacion', 'hidrocarburos', 'legumbres', 'azucar', 'carnica-porcina', 'limon', 'lactea', 'fruticola-citricos-dulces', 'carnica-vacuna', 'fruticola-manzana-y-pera', 'ovinos-lana-y-carne', 'software-y-servicios-informaticos', 'oleaginosa', 'servicios-de-investigacion-y-desarrollo', 'industrias-culturales', 'petroquimica-plastica', 'carnica-aviar', 'yerba-mate', 'pesca-y-puertos-pesqueros', 'olivicola']
 URL_BASE = f'https://github.com/{OWNER}/{REPO}/releases/download/{TAG}/'
 
+# Columns to translate in cleaning process
+columns_to_translate = ['sector_name', 'name_frequency', 'coverage_name', 'reach_type']
+
 def clean_format_data(df: pd.DataFrame) -> pd.DataFrame:
     """Cleans and formats data before loading."""
+    # Renaming columns
     df.columns = [
         'sector_id', 'sector_name', 'variable_id', 'activity_product_name', 
         'indicator', 'unit_of_measure', 'source', 'name_frequency', 
@@ -31,16 +37,36 @@ def clean_format_data(df: pd.DataFrame) -> pd.DataFrame:
                    .str.decode('utf-8')
                    .str.lower()
     )
-    
+
     # Drop column and fill NaN values
     df = df.drop(columns=['reach_id'], errors='ignore')
     df['value'] = df['value'].fillna(0)
+    
+    # Round 'value' to 2 decimal places
+    df['value'] = df['value'].round(2)
+
+    # Translate columns content using the mapping dictionaries
+    mapping_index = 0
+
+    for column in columns_to_translate:    
+        m = map_list[mapping_index]
+
+        df[column] = df[column].map(m).fillna(df[column])
+        mapping_index += 1
     
     return df
 
 @dlt.resource(name="economic_sectors", write_disposition="replace")
 def economic_sectors_resource():
-    for sector in SECTORS:
+    # Output path for the generated CSV file
+    output_path = "data/sectors.csv"
+    os.makedirs("data", exist_ok=True)
+    
+    # If the output file already exists, remove it to avoid appending to old data
+    if os.path.exists(output_path):
+        os.remove(output_path)
+
+    for i, sector in enumerate(SECTORS):
         filename = f"{sector}.csv"
         download_url = f"{URL_BASE}{filename}"
         
@@ -53,6 +79,12 @@ def economic_sectors_resource():
         
         # Transform before loading
         df_cleaned = clean_format_data(df)
+
+        # WRITE TO CSV:
+        # - mode='a' to sector append to the file (instead of overwriting it).
+        # - header=True to keep the header only in the first iteration (avoid repeating it).
+        first_row = (i == 0)
+        df_cleaned.to_csv(output_path, mode='a', index=False, header=first_row)
         
         yield df_cleaned
 
---
+++ economic-sectors/translate_dicts.py
@@ -0,0 +1,68 @@
+map_sector_name = {
+    'oleaginosa': 'oilseeds',
+    'pesca y puertos pesqueros': 'fishing and fishing ports',
+    'maiz': 'corn',
+    'trigo': 'wheat',
+    'comercio interno': 'domestic trade',
+    'hidrocarburos': 'hydrocarbons',
+    'servicios turisticos': 'tourism services',
+    'vitivinicultura': 'viticulture',
+    'carnica - vacuna': 'beef industry',
+    'algodonera - textil': 'cotton and textiles',
+    'construccion': 'construction',
+    'legumbres': 'pulses',
+    'ovinos - lana y carne': 'sheep - wool and meat',
+    'arroz': 'rice',
+    'carnica - porcina': 'pork industry',
+    'energias alternativas': 'alternative energies',
+    'automotriz y autopartista': 'automotive and auto parts',
+    'tabaco': 'tobacco',
+    'forestal, papel y muebles': 'forestry, paper and furniture',
+    'carnica - aviar': 'poultry industry',
+    'yerba mate': 'yerba mate',
+    'fruticola - manzana y pera': 'fruit - apple and pear',
+    'lactea': 'dairy',
+    'azucar': 'sugar',
+    'te': 'tea',
+    'maquinaria agricola': 'agricultural machinery',
+    'fruticola - citricos dulces': 'fruit - sweet citrus',
+    'telecomunicaciones': 'telecommunications',
+    'mineria metalifera y rocas de aplicacion': 'metal mining and industrial rocks',
+    'limon': 'lemon',
+    'apicola': 'beekeeping',
+    'servicios de investigacion y desarrollo': 'R&D services',
+    'industrias culturales': 'cultural industries',
+    'fruticola - fruta de carozo': 'fruit - stone fruit',
+    'petroquimica - plastica': 'petrochemicals and plastics',
+    'software y servicios informaticos': 'software and IT services',
+    'olivicola': 'olive growing'
+}
+
+map_name_frequency = {
+    'anual': 'annual',
+    'mensual': 'medium frequency',
+    'trimestral': 'medium frequency',
+}
+
+map_coverage_name = {
+    'nacional': 'national',
+    'mundial': 'universal',
+    'region protegida patagonica': 'Patagonian protected region',
+    'provincial': 'provincial'
+}
+
+map_reach_type = {
+    'departamento': 'department',
+    'provincia': 'province',
+    'pais': 'country',
+    'puerto': 'port',
+    'localidad': 'locality',
+    'mundo': 'world',
+    'region protegida patagonica': 'patagonian protected region',
+    'partido': 'district',
+    'ingenio': 'mill',
+    'estado nacional': 'national government',
+    'parques nacionales': 'national parks'
+}
+
+map_list = [map_sector_name, map_name_frequency, map_coverage_name, map_reach_type]
